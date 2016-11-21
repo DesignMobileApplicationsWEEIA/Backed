b0c62609fa4fda1187ff8a098d5197497f656421
Dominik Kotecki <dominikus1910@gmail.com>
2016-11-21 19:38:10 +0100

    add PlaceController

---
+++ src/Web/Controllers/PlaceController.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Model.Api;
+using Domain.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class PlaceController : Controller
+    {
+        private readonly IPlaceService _placeService;
+
+        public PlaceController(IPlaceService placeService)
+        {
+            _placeService = placeService;
+        }
+
+        [HttpPost]
+        public Result<bool> Search([FromBody] ApiPlace place)
+        {
+            return _placeService.Add(place, "");
+        }
+
+    }
+}
