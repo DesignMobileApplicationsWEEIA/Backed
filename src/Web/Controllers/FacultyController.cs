7bf47a63bbccb77d044df1a5bf9b7921d2b2ec27
Dominik Kotecki <dominikus1910@gmail.com>
2016-11-23 22:01:03 +0100

    add FacultyController

--- src/Web/Controllers/FacultyController.cs
+++ src/Web/Controllers/FacultyController.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Domain.Cache.Interfaces;
+using Domain.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Web.Controllers
 {
     [Route("api/[controller]")]
     public class FacultyController : Controller
     {
-        
+        private readonly IFacultyService _facultyService;
+        private readonly ICacheService _cacheService;
+
+        public FacultyController(IFacultyService facultyService, ICacheService cacheService)
+        {
+            _facultyService = facultyService;
+            _cacheService = cacheService;
+        }
     }
 }
