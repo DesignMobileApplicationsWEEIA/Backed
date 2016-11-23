313960659fdde4b3d77ba5108d797c3f42a1d81f
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-11-23 12:02:33 +0100

    add CTi to seed

--- src/Web/Database/Implementation/Seed.cs
+++ src/Web/Database/Implementation/Seed.cs
@@ -14,7 +14,7 @@ namespace Web.Migrations
                 return;
             }
 
-            var weeiaLogo = new Logo()
+            var weeiaLogo = new Logo
             {
                Content = null,
                ContentType = "png",
@@ -23,27 +23,60 @@ namespace Web.Migrations
 
             var logoRes = context.Logos.Add(weeiaLogo);
 
-            var building = context.Buildings.Add(new Building()
+            var weeia = context.Buildings.Add(new Building
             {
                 Address = "Bohdana Stefanowskiego 18/22, Łódź",
                 Description = "Wydział Elektrotechniki, Elektroniki, Informatyki i Automatyki Politechniki Łódzkiej",
                 Name = "Weeia"
             });
 
-            context.Faculties.Add(new Faculty()
+            context.Faculties.Add(new Faculty
             {
                 LogoId = logoRes.Entity.Id,
                 Name = "WEEIA_LOGO",
                 ShortName = "weeia",
-                BuildingId = building.Entity.Id
+                BuildingId = weeia.Entity.Id
             });
 
-            var weeiaPlaces = new List<Place>()
+            var weeiaPlaces = new List<Place>
             {
-                new Place() {Latitude = 51.752497, Longitude = 19.453010, BuildingId = building.Entity.Id},
-                new Place() {Latitude = 51.75532, Longitude = 19.453509, BuildingId = building.Entity.Id},
+                new Place {Latitude = 51.752497, Longitude = 19.453010, BuildingId = weeia.Entity.Id},
+                new Place {Latitude = 51.75532, Longitude = 19.453509, BuildingId = weeia.Entity.Id},
             };
             context.Places.AddRange(weeiaPlaces);
+
+
+
+            var ctiLogo = new Logo
+            {
+                Content = null,
+                ContentType = "png",
+            };
+
+
+            var citlogores = context.Logos.Add(ctiLogo);
+
+            var cti = context.Buildings.Add(new Building
+            {
+                Address = "B-19, Wólczańska 217/223, 90-924 Łódź",
+                Description = "Centrum Technologii Informatycznych – ogólnouczelniana jednostka Politechniki Łódzkiej. Zadaniem Centrum Technologii Informatycznych jest wspieranie kształcenia w zakresie budowania i wykorzystywania",
+                Name = "CTI"
+            });
+
+            context.Faculties.Add(new Faculty
+            {
+                LogoId = citlogores.Entity.Id,
+                Name = "CTI_LOGO",
+                ShortName = "CTI",
+                BuildingId = cti.Entity.Id
+            });
+
+            var ctiPlaces = new List<Place>
+            {
+                new Place {Latitude = 51.747054, Longitude = 19.455819, BuildingId = cti.Entity.Id},
+                new Place {Latitude = 51.747364, Longitude = 19.455817, BuildingId = cti.Entity.Id},
+            };
+            context.Places.AddRange(ctiPlaces);
             context.SaveChanges();
         }
     }
