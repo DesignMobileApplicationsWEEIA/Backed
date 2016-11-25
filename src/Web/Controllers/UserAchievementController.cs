574eafc3a9cfcc1ef269c485303f79d8bac8f637
Dominik Kotecki <dominikus1910@gmail.com>
2016-11-25 15:35:09 +0100

    add UserAchievementController methods

--- src/Web/Controllers/UserAchievementController.cs
+++ src/Web/Controllers/UserAchievementController.cs
@@ -1,11 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Domain.Cache.Interfaces;
+using Domain.Model.Api;
+using Domain.Model.Database;
+using Domain.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers
 {
-    public class UserAchievementController
+    [Route("api/[controller]")]
+    public class UserAchievementController : Controller
     {
+        private readonly IUserAchievementService _userAchievementService;
+        private readonly ICacheService _cacheService;
+
+        public UserAchievementController(IUserAchievementService userAchievementService, ICacheService cacheService)
+        {
+            _userAchievementService = userAchievementService;
+            _cacheService = cacheService;
+        }
+
+        [HttpGet]
+        public Result<IEnumerable<UserAchievement>> Get()
+        {
+            string key = $"{nameof(UserAchievementController)}-{nameof(Get)}";
+            return _cacheService.GetOrStore(key, () => _userAchievementService.GetAll(), TimeSpan.FromMinutes(1));
+        }
+
+        [HttpPost]
+        public Result<bool> Post(ApiUserAchievement userAchievement)
+        {
+            return _userAchievementService.Add(userAchievement);
+        }
     }
 }
